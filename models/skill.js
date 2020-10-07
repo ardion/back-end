7ee3d84615463387de782b318f2440dd7407799d
ardion <ardionm97@gmail.com>
2020-10-08 00:23:05 +0800

    fourth commit

--- models/skill.js
+++ models/skill.js
@@ -3,7 +3,7 @@ module.exports = {
 
   createSkillModel: (setData) => {
     return new Promise((resolve, reject) => {
-      db.query('INSERT INTO table_Skill SET ?', setData, (err, result) => {
+      db.query('INSERT INTO table_skill SET ?', setData, (err, result) => {
         if (!err) {
           resolve(result)
         } else {
@@ -15,7 +15,7 @@ module.exports = {
 
   getDataSkillByIDModel: (id) => {
     return new Promise((resolve, reject) => {
-      db.query(`SELECT*FROM table_Skill WHERE id_skill=${id}`, (err, result, field) => {
+      db.query(`SELECT*FROM table_skill WHERE id_skill=${id}`, (err, result, field) => {
         if (!err) {
           resolve(result)
         } else {
@@ -27,7 +27,7 @@ module.exports = {
 
  getDataSkillModel: (searchKey, searchValue, limit, offset) => {
     return new Promise((resolve, reject) => {
-      db.query(`SELECT * FROM table_Skill WHERE ${searchKey} LIKE '%${searchValue}%' LIMIT ${limit} OFFSET ${offset}`, (err, result, field) => {
+      db.query(`SELECT * FROM table_skill WHERE ${searchKey} LIKE '%${searchValue}%' LIMIT ${limit} OFFSET ${offset}`, (err, result, field) => {
         if (!err) {
           resolve(result)
         } else {
@@ -39,9 +39,9 @@ module.exports = {
 
   updateSkillModel: (arr, idSkill) => {
     return new Promise((resolve, reject) => {
-      db.query(`SELECT * FROM table_Skill WHERE id_skill = ${idSkill}`, (_err, result, _field) => {
+      db.query(`SELECT * FROM table_skill WHERE id_skill = ${idSkill}`, (_err, result, _field) => {
         if (result.length) {
-          db.query(`UPDATE table_Skill SET id_worker ='${arr[0]}', skill='${arr[1]}'
+          db.query(`UPDATE table_skill SET id_worker ='${arr[0]}', skill='${arr[1]}'
            WHERE id_skill = ${idSkill}`, (_err, result, _fields) => {
             if (!_err) {
               resolve(result)
@@ -56,7 +56,7 @@ module.exports = {
 
   patchSkillModel: (data, idSkill) => {
     return new Promise((resolve, reject) => {
-      var query = `UPDATE table_Skill SET ${data} WHERE id_skill = ${idSkill}`
+      var query = `UPDATE table_skill SET ${data} WHERE id_skill = ${idSkill}`
       db.query(query, (_err, result, _field) => {
         if (!_err) {
           resolve(result)
@@ -69,7 +69,7 @@ module.exports = {
 
    deleteSkillModel: (idSkill) => {
     return new Promise((resolve, reject) => {
-      db.query(`DELETE FROM table_Skill WHERE id_skill=${idSkill}`, (err, result, field) => {
+      db.query(`DELETE FROM table_skill WHERE id_skill=${idSkill}`, (err, result, field) => {
         if (!err) {
           resolve(result)
         } else {
